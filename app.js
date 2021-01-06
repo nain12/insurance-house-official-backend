9789d615e64b4bcea27b36a8960088c38d5a3da0
nain12 <nainysewaney@gmail.com>
2021-01-06 09:55:36 +0530

    Add cors package to require

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require('cors');
 
 const adminRoutes = require("./routes/admin");
 const userRoutes = require("./routes/user");
