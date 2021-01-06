cbbf5b3a1e2c50457b2cfc147448219c4dbe49ab
nain12 <nainysewaney@gmail.com>
2021-01-06 10:44:59 +0530

    Add X-Requested with header in response

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
   }
   //res.setHeader("Access-Control-Allow-Origin", "https://nain12.github.io");
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
   res.setHeader("Access-Control-Allow-Credentials", "true");
   next();
 });
