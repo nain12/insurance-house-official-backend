b0613d2c75608144469b290670b36c642db4ad74
nain12 <nainysewaney@gmail.com>
2021-01-07 09:49:29 +0530

    Add response to console

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, Origin");
   res.setHeader("Access-Control-Expose-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, Origin");
   res.setHeader("Access-Control-Allow-Credentials", "true");
-  console.log('Response Headers', res.headers);
+  console.log('Response Headers', res);
   next();
 });
 
